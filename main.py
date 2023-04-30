587146c886fc9445288182464006cb9e1198a09a
kloklee88 <kloklee888@gmail.com>
2023-04-30 06:18:56 +0000

    Made working changes to bot

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from keep_alive import keep_alive
 #Flask Server to keep repl.it alive
 keep_alive()
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True
 intents.presences = True
+intents.message_content = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -17,10 +19,11 @@ async def on_ready():
   await client.change_presence(activity=activity)
   print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
-      return
+    return
 
   #Process message commands
   full_command = message.content.strip().split(' ')
@@ -30,15 +33,18 @@ async def on_message(message):
     if len(full_command) == 2:
       first_command = full_command[1]
       if first_command.startswith('<@!'):
-        await message.channel.send(geek_service.get_rankings(message.mentions[0]))
+        await message.channel.send(
+          geek_service.get_rankings(message.mentions[0]))
       elif first_command == 'about':
         await message.channel.send(about())
       elif first_command == 'help':
         await message.channel.send(geek_service.get_help())
-      elif first_command == 'quack':
+      elif first_command == 'collection':
         await message.channel.send(geek_service.get_collection())
+
 
 def about():
   return "A dedicated Discord bot for He's a Board Game Group server for everything, anything, and nothing :smile:"
+
 
 client.run(os.getenv('TOKEN'))
